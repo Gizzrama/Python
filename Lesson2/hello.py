dedf519b9f3621dd35657b6eab22617988ff1b9e
Gizzrama <gizzrama0710@gmail.com>
2026-07-14 12:43:45 +0530

    Update Code

---
+++ Lesson2/hello.py
@@ -0,0 +1,6 @@
+print("Welcome to python world")
+age = "14"
+print("age")
+print(age)
+#This is a print statement
+print("\nHello",age)
---
+++ Lesson2/variable.py
@@ -0,0 +1,15 @@
+x = 5
+y = "7"
+z = 9
+a = "11"
+firstName = "Srihari"
+lastName = "Tejaswy"
+print("The value of x is",x)
+print("The value of y is",y)
+print(f"The value of x is {x}")
+print(x + z)
+print(firstName + lastName)
+print(y + a)
+name = input("Please write your name")
+print("Hello", name,"\nWelcome to Codingal")
+print(f"Hello {name} Welcome to Codingal")
