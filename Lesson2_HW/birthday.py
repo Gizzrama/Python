0993046f43ca9bab76ec1c97f16633ed1233b11e
Gizzrama <gizzrama0710@gmail.com>
2026-07-15 13:03:27 +0530

    Update Code

---
+++ Lesson2_HW/birthday.py
@@ -0,0 +1,16 @@
+friend1 = "20/11/2010|Allison"
+
+#Second Friend
+friend2 = "14/3/2011|Bella"
+
+#Third Friend
+friend3 = "12/10/2010|Caitlyn"
+
+#Fourth Friend
+friend4 = "05/12/2010|Donovan"
+
+#Fifth Friend
+friend5= "13/3/2011|Evelyn"
+
+print("Hey there! These are my friends' names and birthdays:")
+print(f"\n{friend1}, {friend2}, {friend3}, {friend4}, {friend5}")
