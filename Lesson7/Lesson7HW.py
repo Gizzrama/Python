092a5ab2c7e4ebddec85cf6ef9cca283cf0bb113
Gizzrama <gizzrama0710@gmail.com>
2026-07-27 12:44:47 +0530

    Updated Code

--- Lesson7/Lesson7HW.py
+++ Lesson7/Lesson7HW.py
@@ -1,14 +1,20 @@
-
+#ASCII is a standardized character encoding system that assigns a unique number between 0 and 127 to English letters, 
+#digits, and symbols to ensure universal text compatibility across different computers. It acts as a critical middleman, 
+#translating human-typed letters into decimal numbers that software can easily organize. While binary is the raw, foundational 
+#language of 1s and 0s that hardware processes, ASCII serves as the rulebook that gives those binary sequences their specific text
+#meanings. Today, this system is widely used in computer programming source files, network communication protocols, and as the 
+#structural foundation for modern Unicode and emojis.
 print("ASCII String Value Checker")
 
-#print(ord("b")) --> The ord
-#print(ord("5"))
+#print(ord("b")) --> The ord() function returns the ASCII value for a character
+#print(ord("5")) --> The chr() function converts an ASCII value into a character
 
 #print(chr(78))
 #print(chr(90))
 
 character = input("Enter a single character: ").strip()
 
+#len() is a function in python which checks the amount of characters in a string or an integer. It does not do the same for float.
 if type(character) is str or int and len(character) == 1:
     print("Valid input")
         
@@ -29,7 +35,14 @@ if type(character) is str or int and len(character) == 1:
         print("This is the spacebar")
 
     else:
-        print("It is a special character")
+        print("It is a special character") 
 
 else:
     print("Enter only one character. Try again")
+
+#ASCII Ranges
+# 65 - 90: A - Z (uppercase)
+# 97 - 122: a - z (lowercase)
+# 48 - 57: 0 - 9 (digits)
+# 32: Spacebar
+# Everythin else: Special characters
---
+++ Lesson9/ride.py
@@ -0,0 +1,83 @@
+print("We are from ride building company")
+print("We provide bikes, cars and scooters.")
+choice = int(input("Enter 1 for car, 2 for bike and 3 for scooter."))
+
+if choice == 1:
+    print("Bike chosen")
+    print("We have three types of bikes:")
+    print('1) Dirt Bike')
+    print("2) Motorbike")
+    print("3) Mountain Bike")
+    choice = int(input("Enter 1 for Dirt bike, 2 for motorbike and 3 for mtb"))
+   
+    if choice == 1:
+        print("You have chosen dirt bike.")
+        print("Top speed: 150 km/h")
+        print("All terrain")
+        print("Cost 2000 Rs/- for 1 hour")
+    
+    elif choice == 2:
+        print("You have chosen motorbike bike.")
+        print("Top speed: 240 km/h")
+        print("Smooth on roads and dirt roads")
+        print("Cost 3000 Rs/- for 1 hour")
+
+    elif choice == 3:
+        print("You have chosen MTB bike.")
+        print("Top speed: 170 km/h")
+        print("Versatile")
+        print("Cost 3000 Rs/- for 1 hour")
+    
+    else:
+        print("Enter a valid number")
+
+
+elif choice == 2:
+
+    print("Car chosen")
+    print("We have three types of cars:")
+    print('1) Compact Car')
+    print("2) SUV")
+    print("3) Jeep")
+    choice = int(input("Enter 1 for compact car, 2 for SUV and 3 for Jeep"))
+   
+    if choice == 1:
+        print("You have chosen compact car.")
+        print("Top speed: 200 km/h")
+        print("Smooth on roads and dirt roads")
+        print("Cost 3000 Rs/- for 1 hour")
+    
+    elif choice == 2:
+        print("You have chosen SUV.")
+        print("Top speed: 240 km/h")
+        print("Smooth on roads and dirt roads")
+        print("Cost 4000 Rs/- for 1 hour")
+
+    elif choice == 3:
+        print("You have chosen Jeep.")
+        print("Top speed: 220 km/h")
+        print("Versatile")
+        print("Cost 4000 Rs/- for 1 hour")
+
+elif choice == 3:
+    print("Scooter chosen")
+    print("For 1 hour, it will cost 2000 Rs/-")
+    hours = int(input("For how many hours do you want to use our scooter"))
+  
+    total_cost = hours * 2000
+    gear_kit = int(input("Do you want a gearkit? Enter 1 for yes and 2 for no."))
+   
+    if gear_kit == 1:
+        print("It will cost an additional 2000 Rs/- for modifications for seats and handles")
+        new_total_cost = total_cost + 2000
+    
+    else:
+        print("No modifications required")
+    
+    print(f"The total price for {hours} hours is {total_cost}. The total price for additional modification for {hours} hours is {new_total_cost}")
+
+
+
+else:
+    print("Please enter a valid number from 1 - 3.")
+
