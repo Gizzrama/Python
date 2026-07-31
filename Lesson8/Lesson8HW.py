805913c8d67e484fe2fc30a6faed94322a1ddd4b
Gizzrama <gizzrama0710@gmail.com>
2026-07-31 11:32:03 +0530

    Updated Code

--- Lesson8/Lesson8HW.py
+++ Lesson8/Lesson8HW.py
@@ -1,13 +1,66 @@
-#Hypothetical Mathematical Problem --> 18/3 + 4*8 = 38
-#Tiya's Problem --> 4/3 + 38*8 = 18
-input("Enter your name, the problem and ")
-print("Three of the values have been misplaced.")
-mistake1 = 18 #this line is just so the person reading this code can understand my thought process.
-print(f"The program has discovered that {mistake1} should not be where it is")
-mistake2 = 
-print(4/3 + 38*8 == 18)
+#GROCERY COST COMPARISON TOOL
+rice_price = 17
+milk_price = 8
+fruit_price = 12
+baskets = 3
+members = int(input("How many people are in your family including pets? "))
+
+basket_cost_per_person = (rice_price + milk_price + fruit_price) * baskets / members
+print("Basket cost per person: ", basket_cost_per_person)
+
+grocery_items = int(input("enter the total number of grocery items: "))
+
+if grocery_items % members == 0:
+    print(f"All the items can be shared equally among {members} people")
+
+else:
+    print(f"The total number of items cannot be shared equally among {members} people.")
 
 
+recorded_average = 25
+incorrect_weekly_cost = 35
+correct_weekly_cost = 38
+weeks = 3
 
+recorded_total = recorded_average * weeks
+print("The recorded total is: ", recorded_total)
+
+correct_total = recorded_total - incorrect_weekly_cost + correct_weekly_cost
+correct_average = correct_total / weeks
+print("The correct total is: ", correct_total) 
+
+Store_A = 23
+Store_B = 26
+Store_C = 25
+
+if correct_average < Store_A:
+    print("The average is less than store A's average.")
+
+elif correct_average > Store_A:
+    print("The average is greater than store A's average.")
+
+elif correct_average == Store_A:
+    print("The average is equal to store A's average.")
+
+elif correct_average < Store_B:
+    print("The average is less than store B's average.")
+
+elif correct_average > Store_B:
+    print("The average is greater than store B's average.")
+
+elif correct_average == Store_B:
+    print("The average is equal to store B's average.")
+
+elif correct_average < Store_C:
+    print("The average is less than store C's average.")
+
+elif correct_average > Store_C:
+    print("The average is greater than store C's average.")
+
+elif correct_average == Store_C:
+    print("The average is equal to store C's average.")
+
+else:
+    print("Enter a integer or float value")
 
 
