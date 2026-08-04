ecc523d28957c3b0d8c09a6588ae30c6990d9b23
Gizzrama <gizzrama0710@gmail.com>
2026-08-04 18:11:00 +0530

    Updated Code

--- Lesson11/Lesson11HW.py
+++ Lesson11/Lesson11HW.py
@@ -1,4 +1,4 @@
-total_homework = 4
+total_homework = 3
 completed_count = 0
 task_num = 1
 
@@ -13,27 +13,53 @@ while task_num <= total_homework:
         print("English Homework")
     
     else:
-        print("Have some freetime")
+        print("Please enter numbers 1 - 3 only.")
+        continue
     
-    task_status = input("Is your task finished? Enter yes or no")
+    task_status = input("Is your task finished? Enter yes or no: ").strip().lower()
 
     if task_status == "yes":
         print("Good Job!")
         completed_count+=1
         task_num+=1
-        print(total_homework - completed_count)
+        print(f"Tasks Remaining: {total_homework - completed_count}")
+        
+        
 
     elif task_status == "no":
         print("You got this.")
+        
+        
     
     else:
         print("Please enter either yes or no.")
+        continue
+        
+print("\n")     
+print("ALL HOMEWORK COMPLTETED! GOOD JOB!")
+print("\n")
+print("Here is an example of a while loop with a condition that does not change on its own.")
 
-
-print("Good work! You have completed all your homework!")
-
+value = 1
 safety_count = 0
 
-while True:
+
+while value == 1:
+    print("This is a never ending loop since value is always equal to 1.")
+    safety_count += 1
+
+    if safety_count == 3:
+        print("This loop will now break since the safety count is incremented by one after each comment, and")
+        print("this if conditional statement tells us that if the safety count reached 3, then the code should break, which happened as you were reading this.")
+        break
+
+
+print("---THE FINAL SUMMARY---")
+print("\n")
+print("The original task count is ", total_homework)
+print("The completed task count is ", completed_count)
+print("The remaining task count is ", total_homework - completed_count)
+
+
     
     
